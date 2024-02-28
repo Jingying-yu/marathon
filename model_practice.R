13c7b8f599145b45861c4a761c6ebe5626127a6f
Jingying Yu <jingying.yu@mail.utoronto.ca>
2024-02-28 01:59:12 +0000

    rearranged location of folders and files. Finished first model and saved

---
+++ model_practice.R
@@ -0,0 +1,28 @@
+#### Setup ####
+library(tidyverse)
+library(rstanarm)
+
+
+#### Read Data ####
+
+analysis_data <- read.csv("data/analysis_data/analysis_data.csv")
+
+
+#### Model Data ####
+#first model
+first_model <-
+  stan_glm(
+    formula = flying_time ~ length + width,
+    data = analysis_data,
+    family = gaussian(),
+    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_aux = exponential(rate = 1, autoscale = TRUE),
+    seed = 853
+  )
+
+# Save model
+saveRDS(
+  first_model,
+  file = "models/first_model.rds"
+)
